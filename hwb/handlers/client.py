5cfe0df48d90c048de7fe42d5930bba9efe1b735
tmutarakan <ratibor.tmutarakansky@yandex.ru>
2022-07-03 13:05:49 +0300

    refactor callback_query_handler
    Созданы отдельные функции для обработки основных фильтров

--- hwb/handlers/client.py
+++ hwb/handlers/client.py
@@ -6,27 +6,47 @@ from sqlite.sqlite_db import sql_read, sql_path, sql_parent
 
 
 async def commands_start(message: types.Message):
-    await message.answer(f'{message.from_user.full_name} вас приветствует справочная.', reply_markup=kc.create_keyboard(parent='help'))
+    await message.answer(f'{message.from_user.full_name} вас приветствует справочная.', reply_markup=kc.create_keyboard(parent='root'))
+
+
+async def root_callback_button(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(
+        callback_query.from_user.id,
+        f'{callback_query.from_user.full_name} вас приветствует бот - справочная.',
+        reply_markup=kc.create_keyboard(parent='root')
+        )
+    await bot.edit_message_reply_markup(
+        chat_id=callback_query.from_user.id,
+        message_id=callback_query.message.message_id,
+        reply_markup=None)
+
+
+async def file_callback_button(callback_query: types.CallbackQuery):
+    data = callback_query.data
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_document(callback_query.from_user.id, InputFile(sql_path(data)), reply_markup=kc.create_keyboard(parent=data))
+    await bot.edit_message_reply_markup(
+        chat_id=callback_query.from_user.id,
+        message_id=callback_query.message.message_id,
+        reply_markup=None)
+
+
+async def photo_callback_button(callback_query: types.CallbackQuery):
+    data = callback_query.data
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_photo(callback_query.from_user.id, InputFile(sql_path(data)), caption=sql_read(data), reply_markup=kc.create_keyboard(parent=data))
+    await bot.edit_message_reply_markup(
+        chat_id=callback_query.from_user.id,
+        message_id=callback_query.message.message_id,
+        reply_markup=None)
 
 
 async def process_callback_button(callback_query: types.CallbackQuery):
     data = callback_query.data
     await bot.answer_callback_query(callback_query.id)
-    if data == 'help':
-        # Возврат к началу
-        await bot.send_message(
-            callback_query.from_user.id,
-            f'{callback_query.from_user.full_name} вас приветствует справочная.',
-            reply_markup=kc.create_keyboard(parent='help'))
-    elif '_file' in data:
-        await bot.send_document(callback_query.from_user.id, InputFile(sql_path(data)), reply_markup=kc.create_keyboard(parent=data))
-        # data = sql_parent(data)
-        # await bot.send_message(callback_query.from_user.id, sql_read(data), reply_markup=kc.create_keyboard(parent=data))
-    elif '_photo' in data:
-        await bot.send_photo(callback_query.from_user.id, InputFile(sql_path(data)), caption=sql_read(data), reply_markup=kc.create_keyboard(parent=data))
-    else:
-        # Отправляет сообщение с новой клавиатурой
-        await bot.send_message(callback_query.from_user.id, sql_read(data), reply_markup=kc.create_keyboard(parent=data))
+    # Отправляет сообщение с новой клавиатурой
+    await bot.send_message(callback_query.from_user.id, sql_read(data), reply_markup=kc.create_keyboard(parent=data))
     # Удаляет клавиатуру предыдущего сообщения
     await bot.edit_message_reply_markup(
         chat_id=callback_query.from_user.id,
@@ -36,4 +56,7 @@ async def process_callback_button(callback_query: types.CallbackQuery):
 
 def register_handlers_client(dp: Dispatcher):
     dp.register_message_handler(commands_start, commands=['start', 'help'])
+    dp.register_callback_query_handler(root_callback_button, lambda callback_query: callback_query.data == 'root')
+    dp.register_callback_query_handler(file_callback_button, lambda callback_query: '_file' in callback_query.data)
+    dp.register_callback_query_handler(photo_callback_button, lambda callback_query: '_photo' in callback_query.data)
     dp.register_callback_query_handler(process_callback_button)
