2b06982f53985a9104415a20732759e891023c1e
tmutarakan <ratibor.tmutarakansky@yandex.ru>
2022-07-03 15:18:38 +0300

    refactor callback_query_handler
    Удаление клавиатуры вынесено в отдельную функцию

--- hwb/handlers/client.py
+++ hwb/handlers/client.py
@@ -9,6 +9,14 @@ async def commands_start(message: types.Message):
     await message.answer(f'{message.from_user.full_name} вас приветствует справочная.', reply_markup=kc.create_keyboard(parent='root'))
 
 
+async def delete_prev_markup(user_id, message_id):
+    # Удаляет клавиатуру предыдущего сообщения
+    await bot.edit_message_reply_markup(
+        chat_id=user_id,
+        message_id=message_id,
+        reply_markup=None)
+
+
 async def root_callback_button(callback_query: types.CallbackQuery):
     await bot.answer_callback_query(callback_query.id)
     await bot.send_message(
@@ -16,30 +24,21 @@ async def root_callback_button(callback_query: types.CallbackQuery):
         f'{callback_query.from_user.full_name} вас приветствует бот - справочная.',
         reply_markup=kc.create_keyboard(parent='root')
         )
-    await bot.edit_message_reply_markup(
-        chat_id=callback_query.from_user.id,
-        message_id=callback_query.message.message_id,
-        reply_markup=None)
+    await delete_prev_markup(callback_query.from_user.id, callback_query.message.message_id)
 
 
 async def file_callback_button(callback_query: types.CallbackQuery):
     data = callback_query.data
     await bot.answer_callback_query(callback_query.id)
     await bot.send_document(callback_query.from_user.id, InputFile(sql_path(data)), reply_markup=kc.create_keyboard(parent=data))
-    await bot.edit_message_reply_markup(
-        chat_id=callback_query.from_user.id,
-        message_id=callback_query.message.message_id,
-        reply_markup=None)
+    await delete_prev_markup(callback_query.from_user.id, callback_query.message.message_id)
 
 
 async def photo_callback_button(callback_query: types.CallbackQuery):
     data = callback_query.data
     await bot.answer_callback_query(callback_query.id)
     await bot.send_photo(callback_query.from_user.id, InputFile(sql_path(data)), caption=sql_read(data), reply_markup=kc.create_keyboard(parent=data))
-    await bot.edit_message_reply_markup(
-        chat_id=callback_query.from_user.id,
-        message_id=callback_query.message.message_id,
-        reply_markup=None)
+    await delete_prev_markup(callback_query.from_user.id, callback_query.message.message_id)
 
 
 async def process_callback_button(callback_query: types.CallbackQuery):
@@ -47,11 +46,7 @@ async def process_callback_button(callback_query: types.CallbackQuery):
     await bot.answer_callback_query(callback_query.id)
     # Отправляет сообщение с новой клавиатурой
     await bot.send_message(callback_query.from_user.id, sql_read(data), reply_markup=kc.create_keyboard(parent=data))
-    # Удаляет клавиатуру предыдущего сообщения
-    await bot.edit_message_reply_markup(
-        chat_id=callback_query.from_user.id,
-        message_id=callback_query.message.message_id,
-        reply_markup=None)
+    await delete_prev_markup(callback_query.from_user.id, callback_query.message.message_id)
 
 
 def register_handlers_client(dp: Dispatcher):
--- hwb/keyboards/client.py
+++ hwb/keyboards/client.py
@@ -11,26 +11,27 @@ def create_keyboard(parent: str) -> InlineKeyboardMarkup:
 
     while len(msg_list) > 0:
         temp = {
-            'count': 0,
-            'length': 0,
-            'list_length': [],
-            'all_length': True,
-            'list_elem': [],
-            'list_button': []
+            'count': 0,         # Количество кнопок в строке
+            'length': 0,        # Суммарная длина текста кнопок
+            'list_length': [],  # Длина текста каждой кнопки
+            'all_length': True, # Вмещается ли текст всех кнопок в строке
+            'list_button': []   # Список для кнопок
         }
         for elem in msg_list:
             elem_length = len(elem[0])
-            elem_size = 36/(temp['count']+1)
+            elem_size = 36/(temp['count']+1)    # Максимальная длина сообщения в кнопке
             for temp_elem in temp['list_length']:
                 if temp_elem > elem_size:
                     temp['all_length'] = False
-            if temp['length'] + elem_length <= 36 and elem_length <= elem_size and temp['all_length']:
+                    break
+            
+            if temp['length'] + elem_length > 36 or elem_length > elem_size or not temp['all_length']:
+                break                
+            else:
                 temp['count'] += 1
                 temp['length'] += elem_length
                 temp['list_length'].append(elem_length)
-                temp['list_elem'].append(elem)
-            else:
-                break
+            
         for _ in range(temp['count']):
             temp['list_button'].append(InlineKeyboardButton(msg_list[0][0], callback_data=msg_list[0][1]))
             msg_list.pop(0)
