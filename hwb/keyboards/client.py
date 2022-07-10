19ac98aed0ecc76d37e2cd0c3cccf8c918415487
tmutarakan <ratibor.tmutarakansky@yandex.ru>
2022-07-10 17:04:26 +0300

    refactor client keyboard
    Словарь и список для хранения данных заменены на классы.
     Для хранения данных из БД используется очередь.

--- hwb/keyboards/client.py
+++ hwb/keyboards/client.py
@@ -1,41 +1,52 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from sqlite.sqlite_db import sql_child, sql_parent
+from collections import deque
+
+
+class ButtonData:
+    def __init__(self, message, name) -> None:
+        self.message = message
+        self.name = name
+
+
+class TempData:
+    def __init__(self) -> None:
+        self.count = 0              # Количество кнопок в строке
+        self.length = 0             # Суммарная длина текста кнопок
+        self.list_length = []       # Длина текста каждой кнопки
+        self.all_length = True      # Вмещается ли текст всех кнопок в строке
+        self.list_button = []       # Список для кнопок
 
 
 def create_keyboard(parent: str) -> InlineKeyboardMarkup:
     # Создаёт клавиатуру на основе запроса из базы данных
     inline_kbm = InlineKeyboardMarkup()
-    msg_list = []
+    msg_list = deque()
     for name, message in sql_child(parent):
-        msg_list.append([message, name])
+        msg_list.append(ButtonData(message, name))
 
     while len(msg_list) > 0:
-        temp = {
-            'count': 0,         # Количество кнопок в строке
-            'length': 0,        # Суммарная длина текста кнопок
-            'list_length': [],  # Длина текста каждой кнопки
-            'all_length': True, # Вмещается ли текст всех кнопок в строке
-            'list_button': []   # Список для кнопок
-        }
+        temp = TempData()
+        
         for elem in msg_list:
-            elem_length = len(elem[0])
-            elem_size = 36/(temp['count']+1)    # Максимальная длина сообщения в кнопке
-            for temp_elem in temp['list_length']:
+            elem_length = len(elem.message)
+            elem_size = 36/(temp.count + 1)    # Максимальная длина сообщения в кнопке
+            for temp_elem in temp.list_length:
                 if temp_elem > elem_size:
-                    temp['all_length'] = False
+                    temp.all_length = False
                     break
             
-            if temp['length'] + elem_length > 36 or elem_length > elem_size or not temp['all_length']:
+            if temp.length + elem_length > 36 or elem_length > elem_size or not temp.all_length:
                 break                
             else:
-                temp['count'] += 1
-                temp['length'] += elem_length
-                temp['list_length'].append(elem_length)
+                temp.count += 1
+                temp.length += elem_length
+                temp.list_length.append(elem_length)
             
-        for _ in range(temp['count']):
-            temp['list_button'].append(InlineKeyboardButton(msg_list[0][0], callback_data=msg_list[0][1]))
-            msg_list.pop(0)
-        inline_kbm.row(*temp['list_button'])
+        for _ in range(temp.count):
+            b_data = msg_list.popleft()
+            temp.list_button.append(InlineKeyboardButton(b_data.message, callback_data=b_data.name))
+        inline_kbm.row(*temp.list_button)
 
     root = sql_parent(parent)
     if root:
