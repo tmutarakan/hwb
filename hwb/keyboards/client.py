c8161447351dcb43a5f8e635ab5eb5a0ee55d9ff
tmutarakan <ratibor.tmutarakansky@yandex.ru>
2022-07-24 10:24:15 +0300

    add new branch
    Убрана пагинация через базу данных

--- hwb/keyboards/client.py
+++ hwb/keyboards/client.py
@@ -24,9 +24,13 @@ class TempData:
         self.list_button = []       # Список для кнопок
 
 
-class Page:
-    def __init__(self) -> None:
-        self.rows = []
+class State:
+    def __init__(self, rows) -> None:
+        self.page = []
+        if len(rows) < 5:
+            p.rows.append(temp.list_button)
+        else:
+            p.rows.append([ButtonData('Вперёд', '/next')])
 
     def __str__(self) -> str:
         return f'{self.rows}'
@@ -40,7 +44,7 @@ def create_page(parent):
     for name, message in sql_child(parent):
         msg_list.append(ButtonData(message, name))
 
-    p = Page()
+    rows = []
     while len(msg_list) > 0:
         temp = TempData()
         
@@ -62,27 +66,44 @@ def create_page(parent):
         for _ in range(temp.count):
             temp.list_button.append(msg_list.popleft())
 
-        p.rows.append(temp.list_button)
-        #if len(p) < 5:
-        #    p.rows.append(temp.list_button)
-        #else:
-        #    p.rows.append([ButtonData('Вперёд', '/next')])
-        #    break
-    return p
+        rows.append(temp.list_button)
+    return rows
 
 
 def create_keyboard(parent: str) -> InlineKeyboardMarkup:
     # Создаёт клавиатуру на основе запроса из базы данных
     inline_kbm = InlineKeyboardMarkup()
-    page = create_page(parent)
-    for row in page.rows:
+    root = sql_parent(parent)
+    if root:
+        inline_kbm.add(InlineKeyboardButton("Вернуться", callback_data=root))
+    rows = create_page(parent)
+    for row in rows:
         temp = []
         for button_data in row:
             temp.append(InlineKeyboardButton(button_data.message, callback_data=button_data.name))
-        inline_kbm.row(*temp)
+        inline_kbm.row(*temp)    
+    #print(f'{page} {len(page)}')
+    return inline_kbm
 
-    root = sql_parent(parent)
-    print(f'{page} {len(page)}')
+
+
+def edit_keyboard(data):
+    inline_kbm = InlineKeyboardMarkup()
+    root = sql_read_current_parent()
     if root:
         inline_kbm.add(InlineKeyboardButton("Вернуться", callback_data=root))
+
+    pages = sql_read_page(data)
+    row_count = 1
+    temp = []
+    for row_number, name, message in pages:
+        if row_number == row_count:
+            temp.append(InlineKeyboardButton(message, callback_data=name))
+        else:
+            row_count += 1
+            inline_kbm.row(*temp)
+            temp = []
+            temp.append(InlineKeyboardButton(message, callback_data=name))
+    inline_kbm.row(*temp)
+    inline_kbm.row(InlineKeyboardButton('Назад', callback_data='/prev'), InlineKeyboardButton('Вперёд', callback_data='/next'))
     return inline_kbm
