c3133c462aa81a6a86ede85e0e9e640ed3a082cc
tmutarakan <ratibor.tmutarakansky@yandex.ru>
2022-09-18 11:13:24 +0300

    fixed stylistic errors

--- hwb/handlers/client.py
+++ hwb/handlers/client.py
@@ -8,14 +8,10 @@ from config import DEFAULT_PATH_JPG, DEFAULT_PATH_WITHOUT_PHOTO
 
 async def commands_start(message: types.Message):
     user_id = message.from_user.id
-    """await message.answer(
-        f'{message.from_user.full_name} вас приветствует справочная.',
-        reply_markup=kc.create_keyboard(parent='root', user_id=user_id)
-        )"""
     await message.answer_photo(
-        photo = InputFile(DEFAULT_PATH_WITHOUT_PHOTO),
-        caption = f'{message.from_user.full_name} вас приветствует справочная.',
-        reply_markup = kc.create_keyboard(parent='root', user_id=user_id)
+        photo=InputFile(DEFAULT_PATH_WITHOUT_PHOTO),
+        caption=f'{message.from_user.full_name} вас приветствует справочная.',
+        reply_markup=kc.create_keyboard(parent='root', user_id=user_id)
         )
 
 
@@ -32,32 +28,14 @@ async def root_callback_button(callback_query: types.CallbackQuery):
     fn = callback_query.from_user.full_name
     await bot.answer_callback_query(callback_query.id)
     await bot.edit_message_media(
-        media = InputMediaPhoto(
+        media=InputMediaPhoto(
             media=InputFile(DEFAULT_PATH_WITHOUT_PHOTO),
             caption=f'{fn} вас приветствует бот - справочная.'
         ),
-        chat_id = user_id,
+        chat_id=user_id,
         message_id=callback_query.message.message_id,
-        reply_markup = kc.create_keyboard(parent='root', user_id=user_id)
-    )
-    """await bot.send_message(
-        user_id,
-        f'{fn} вас приветствует бот - справочная.',
         reply_markup=kc.create_keyboard(parent='root', user_id=user_id)
-        )
-    await delete_prev_markup(user_id, callback_query.message.message_id)"""
-
-
-"""async def file_callback_button(callback_query: types.CallbackQuery):
-    data = callback_query.data
-    user_id = callback_query.from_user.id
-    await bot.answer_callback_query(callback_query.id)
-    await bot.send_document(
-        user_id,
-        InputFile(get_path(data)),
-        reply_markup=kc.create_keyboard(parent=data, user_id=user_id)
-        )
-    await delete_prev_markup(user_id, callback_query.message.message_id)"""
+    )
 
 
 async def photo_callback_button(callback_query: types.CallbackQuery):
@@ -69,27 +47,14 @@ async def photo_callback_button(callback_query: types.CallbackQuery):
         file = InputFile(DEFAULT_PATH_JPG)
     await bot.answer_callback_query(callback_query.id)
     await bot.edit_message_media(
-        media = InputMediaPhoto(
+        media=InputMediaPhoto(
             media=file,
             caption=get_content(data)
         ),
-        chat_id = user_id,
+        chat_id=user_id,
         message_id=callback_query.message.message_id,
-        reply_markup = kc.create_keyboard(parent=data, user_id=user_id)
-    )
-    #await bot.edit_message_caption(
-    #    caption=get_content(data),
-    #    chat_id=user_id,
-    ##    message_id=callback_query.message.message_id,
-    #    reply_markup = kc.create_keyboard(parent=data, user_id=user_id)
-    #)
-    """await bot.send_photo(
-        user_id,
-        file,
-        caption=get_content(data),
         reply_markup=kc.create_keyboard(parent=data, user_id=user_id)
-        )
-    await delete_prev_markup(user_id, callback_query.message.message_id)"""
+    )
 
 
 async def page_callback_button(callback_query: types.CallbackQuery):
@@ -108,9 +73,9 @@ async def biography_callback_button(callback_query: types.CallbackQuery):
     await bot.answer_callback_query(callback_query.id)
     await bot.edit_message_caption(
         caption=get_content(data),
-        chat_id = user_id,
+        chat_id=user_id,
         message_id=callback_query.message.message_id,
-        reply_markup = kc.create_keyboard(parent=data, user_id=user_id)
+        reply_markup=kc.create_keyboard(parent=data, user_id=user_id)
     )
 
 
@@ -119,24 +84,14 @@ async def process_callback_button(callback_query: types.CallbackQuery):
     user_id = callback_query.from_user.id
     await bot.answer_callback_query(callback_query.id)
     await bot.edit_message_media(
-        media = InputMediaPhoto(
+        media=InputMediaPhoto(
             media=InputFile(DEFAULT_PATH_WITHOUT_PHOTO),
             caption=get_content(data)
         ),
-        chat_id = user_id,
-        message_id=callback_query.message.message_id,
-        reply_markup = kc.create_keyboard(parent=data, user_id=user_id)
-    )
-    """await bot.edit_message_reply_markup(
         chat_id=user_id,
         message_id=callback_query.message.message_id,
-        reply_markup=kc.edit_keyboard(data=data, user_id=user_id))"""
-    # Отправляет сообщение с новой клавиатурой
-    """await bot.send_message(
-        user_id, get_content(data),
         reply_markup=kc.create_keyboard(parent=data, user_id=user_id)
-        )
-    await delete_prev_markup(user_id, callback_query.message.message_id)"""
+    )
 
 
 def register_handlers_client(dp: Dispatcher):
@@ -153,14 +108,6 @@ def register_handlers_client(dp: Dispatcher):
         page_callback_button,
         lambda callback_query: callback_query.data == '/prev'
         )
-    """dp.register_callback_query_handler(
-        page_callback_button,
-        lambda callback_query: isinstance(callback_query.data, int)
-        )"""
-    """dp.register_callback_query_handler(
-        file_callback_button,
-        lambda callback_query: '_file' in callback_query.data
-        )"""
     dp.register_callback_query_handler(
         photo_callback_button,
         lambda callback_query: '_photo' in callback_query.data
@@ -169,9 +116,4 @@ def register_handlers_client(dp: Dispatcher):
         biography_callback_button,
         lambda callback_query: 'bio_' in callback_query.data
         )
-    """dp.register_callback_query_handler(
-        root_callback_button,
-        lambda callback_query: callback_query.data == '/prev'
-        or callback_query.data == '/next', state='*'
-        )"""
     dp.register_callback_query_handler(process_callback_button)
